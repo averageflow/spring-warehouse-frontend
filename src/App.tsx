315f037347a05e373a6f0b22caaec0413fb325e3
averageflow <jjbigorra@gmail.com>
2021-11-21 01:22:55 +0100

    add bootstrap table

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,14 @@ import {
 
 import { useEffect, useState } from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table } from "react-bootstrap";
+
 
 interface Product {
   id: number
   name: string
+  price: number
   createdAt: number
   updatedAt: number
 }
@@ -35,10 +39,25 @@ interface ArticleResponse {
   sort: number[]
 }
 
-function formatProduct(productResponse: ProductResponse): JSX.Element[] {
-  return Object.values(productResponse.sort).map(sortItem => {
-    return <h2>{productResponse.data[sortItem].name}</h2>
-  })
+function formatProduct(productResponse: ProductResponse): JSX.Element {
+  return <Table striped responsive bordered hover>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Created At</th>
+      </tr>
+    </thead>
+    <tbody>
+      {Object.values(productResponse.sort).map(sortItem => {
+        return <tr>
+          <td>{productResponse.data[sortItem].name}</td>
+          <td>{productResponse.data[sortItem].price}</td>
+          <td>{productResponse.data[sortItem].createdAt}</td>
+        </tr>
+      })}
+    </tbody>
+  </Table>
 }
 
 function formatArticle(articleResponse: ArticleResponse): JSX.Element[] {
@@ -51,17 +70,6 @@ function AppView() {
   return (
     <BrowserRouter>
       <div>
-        <ul>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-          <li>
-            <Link to="/articles">Articles</Link>
-          </li>
-        </ul>
-
-        <hr />
-
         <Routes>
           <Route path="/" element={<ProductsView />} />
           <Route path="/products" element={<ProductsView />} />
@@ -71,6 +79,27 @@ function AppView() {
       </div>
     </BrowserRouter>
   );
+}
+
+function AppNavbar(): JSX.Element {
+  return <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Joe's Warehouse</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/products">Products</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/articles">Articles</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav >
 }
 
 
@@ -97,14 +126,14 @@ function ArticlesView(): JSX.Element {
   }, []);
 
   return (
-    <div>
+    <div className="container">
       {articleRows}
     </div>
   );
 }
 
 function ProductsView() {
-  const [productRows, setProductRows] = useState([] as JSX.Element[]);
+  const [productRows, setProductRows] = useState(<></> as JSX.Element);
 
   const fetchProductRows = async () => {
     fetch(
@@ -135,6 +164,7 @@ function ProductsView() {
 function App() {
   return (
     <div>
+      <AppNavbar />
       <AppView />
     </div>
   );
