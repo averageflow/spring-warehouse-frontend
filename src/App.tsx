dc511fce2a271d33fda30213a6daf15e6e792b75
averageflow <jjbigorra@gmail.com>
2021-11-21 01:10:42 +0100

    added rotues

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Route,
+  Routes,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
+
+
+import { useEffect, useState } from 'react';
+
+
+interface Product {
+  id: number
+  name: string
+  createdAt: number
+  updatedAt: number
+}
+
+interface ProductResponse {
+  data: Record<string, Product>
+  sort: number[]
+}
+
+
+interface Article {
+  id: number
+  name: string
+  createdAt: number
+  updatedAt: number
+}
+
+interface ArticleResponse {
+  data: Record<string, Article>
+  sort: number[]
+}
+
+function formatProduct(productResponse: ProductResponse): JSX.Element[] {
+  return Object.values(productResponse.sort).map(sortItem => {
+    return <h2>{productResponse.data[sortItem].name}</h2>
+  })
+}
+
+function formatArticle(articleResponse: ArticleResponse): JSX.Element[] {
+  return Object.values(articleResponse.sort).map(sortItem => {
+    return <h2>{articleResponse.data[sortItem].name}</h2>
+  })
+}
+
+function AppView() {
+  return (
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/articles">Articles</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<ProductsView />} />
+          <Route path="/products" element={<ProductsView />} />
+          <Route path="/articles" element={<ArticlesView />} />
+
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+
+function ArticlesView(): JSX.Element {
+  const [articleRows, setArticleRows] = useState([] as JSX.Element[]);
+
+  const fetchArticleRows = async () => {
+    fetch(
+      `http://localhost:8080/api/articles`,
+      {
+        method: "GET",
+      }
+    )
+      .then(res => res.json())
+      .then(response => {
+        setArticleRows(formatArticle(response))
+      })
+      .catch(error => console.log(error));
+  }
+
+
+  useEffect(() => {
+    fetchArticleRows()
+  }, []);
+
+  return (
+    <div>
+      {articleRows}
+    </div>
+  );
+}
+
+function ProductsView() {
+  const [productRows, setProductRows] = useState([] as JSX.Element[]);
+
+  const fetchProductRows = async () => {
+    fetch(
+      `http://localhost:8080/api/products`,
+      {
+        method: "GET",
+      }
+    )
+      .then(res => res.json())
+      .then(response => {
+        setProductRows(formatProduct(response))
+      })
+      .catch(error => console.log(error));
+  }
+
+  useEffect(() => {
+    fetchProductRows()
+  }, []);
+  return (
+    <div className="container">
+      {productRows}
+    </div>
+  );
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppView />
     </div>
   );
 }
 
 export default App;
+
